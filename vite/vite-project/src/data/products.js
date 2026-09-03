b50fb5147cd572aa5eb0726613ef85beb3670c0f
HARSH KUMAR SHARMA <harshhsharmaa57@gmail.com>
2026-09-03 14:16:03 +0530

    data product

---
+++ vite/vite-project/src/data/products.js
@@ -0,0 +1,11 @@
+import hero from '../assets/hero.png'
+
+const products = [
+  { id: 1, name: 'Comfy Sneakers', price: 49.99, image: hero },
+  { id: 2, name: 'Everyday T-Shirt', price: 19.5, image: hero },
+  { id: 3, name: 'Leather Belt', price: 12.0, image: hero },
+  { id: 4, name: 'Sun Glasses', price: 24.99, image: hero },
+  { id: 5, name: 'Backpack small', price: 39.0, image: hero },
+]
+
+export default products
